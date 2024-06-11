f567cc21221a179eda8b22945719349bbbf47feb
Andrew Barnes <abbarnes852@gmail.com>
2024-06-11 12:54:38 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmCodeChallenge
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        pbSplash = New PictureBox()
+        bg1 = New ComponentModel.BackgroundWorker()
+        pnlTop = New Panel()
+        Label2 = New Label()
+        btnChooseFile = New Button()
+        txtCSVFile = New TextBox()
+        Label1 = New Label()
+        Panel2 = New Panel()
+        pbLoading = New PictureBox()
+        lblStatus = New Label()
+        lstResults = New ListView()
+        CType(pbSplash, ComponentModel.ISupportInitialize).BeginInit()
+        pnlTop.SuspendLayout()
+        Panel2.SuspendLayout()
+        CType(pbLoading, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' pbSplash
+        ' 
+        pbSplash.BackgroundImage = My.Resources.Resources.CodeChallengeLogo
+        pbSplash.BackgroundImageLayout = ImageLayout.Stretch
+        pbSplash.Location = New Point(0, 0)
+        pbSplash.Margin = New Padding(3, 4, 3, 4)
+        pbSplash.Name = "pbSplash"
+        pbSplash.Size = New Size(686, 104)
+        pbSplash.TabIndex = 0
+        pbSplash.TabStop = False
+        ' 
+        ' bg1
+        ' 
+        bg1.WorkerReportsProgress = True
+        ' 
+        ' pnlTop
+        ' 
+        pnlTop.Controls.Add(Label2)
+        pnlTop.Controls.Add(btnChooseFile)
+        pnlTop.Controls.Add(txtCSVFile)
+        pnlTop.Controls.Add(Label1)
+        pnlTop.Dock = DockStyle.Top
+        pnlTop.Location = New Point(0, 0)
+        pnlTop.Margin = New Padding(3, 4, 3, 4)
+        pnlTop.Name = "pnlTop"
+        pnlTop.Size = New Size(686, 133)
+        pnlTop.TabIndex = 1
+        pnlTop.Visible = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(124, 2)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(189, 37)
+        Label2.TabIndex = 3
+        Label2.Text = "Import Loans"
+        ' 
+        ' btnChooseFile
+        ' 
+        btnChooseFile.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        btnChooseFile.Location = New Point(647, 48)
+        btnChooseFile.Name = "btnChooseFile"
+        btnChooseFile.Size = New Size(27, 27)
+        btnChooseFile.TabIndex = 2
+        btnChooseFile.Text = "..."
+        btnChooseFile.UseVisualStyleBackColor = True
+        ' 
+        ' txtCSVFile
+        ' 
+        txtCSVFile.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        txtCSVFile.Location = New Point(124, 48)
+        txtCSVFile.Name = "txtCSVFile"
+        txtCSVFile.ReadOnly = True
+        txtCSVFile.Size = New Size(505, 27)
+        txtCSVFile.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(8, 55)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(110, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Select your file:"
+        ' 
+        ' Panel2
+        ' 
+        Panel2.Controls.Add(pbLoading)
+        Panel2.Controls.Add(lblStatus)
+        Panel2.Dock = DockStyle.Bottom
+        Panel2.Location = New Point(0, 520)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(686, 51)
+        Panel2.TabIndex = 3
+        ' 
+        ' pbLoading
+        ' 
+        pbLoading.BackgroundImageLayout = ImageLayout.Stretch
+        pbLoading.Image = My.Resources.Resources.LoadingImage
+        pbLoading.Location = New Point(13, 8)
+        pbLoading.Name = "pbLoading"
+        pbLoading.Size = New Size(40, 40)
+        pbLoading.SizeMode = PictureBoxSizeMode.StretchImage
+        pbLoading.TabIndex = 4
+        pbLoading.TabStop = False
+        pbLoading.Visible = False
+        ' 
+        ' lblStatus
+        ' 
+        lblStatus.AutoSize = True
+        lblStatus.Location = New Point(81, 16)
+        lblStatus.Name = "lblStatus"
+        lblStatus.Size = New Size(0, 20)
+        lblStatus.TabIndex = 3
+        ' 
+        ' lstResults
+        ' 
+        lstResults.BackColor = Color.AliceBlue
+        lstResults.Dock = DockStyle.Fill
+        lstResults.Font = New Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lstResults.FullRowSelect = True
+        lstResults.Location = New Point(0, 133)
+        lstResults.Name = "lstResults"
+        lstResults.Size = New Size(686, 387)
+        lstResults.TabIndex = 4
+        lstResults.UseCompatibleStateImageBehavior = False
+        lstResults.View = View.Details
+        ' 
+        ' frmCodeChallenge
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(686, 571)
+        Controls.Add(lstResults)
+        Controls.Add(Panel2)
+        Controls.Add(pnlTop)
+        Controls.Add(pbSplash)
+        Font = New Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Margin = New Padding(3, 4, 3, 4)
+        Name = "frmCodeChallenge"
+        Opacity = 0R
+        Text = "Code Challenge - Self Help Credit Union"
+        CType(pbSplash, ComponentModel.ISupportInitialize).EndInit()
+        pnlTop.ResumeLayout(False)
+        pnlTop.PerformLayout()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        CType(pbLoading, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents pbSplash As PictureBox
+    Friend WithEvents bg1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents pnlTop As Panel
+    Friend WithEvents txtCSVFile As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnChooseFile As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents lstResults As ListView
+    Friend WithEvents pbLoading As PictureBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,226 @@
+﻿Imports System.ComponentModel
+
+Public Class frmCodeChallenge
+
+#Region "Loading/Closing"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        FadeIn()
+    End Sub
+
+    Private Sub FadeIn()
+        pbSplash.BringToFront()
+        Me.FormBorderStyle = FormBorderStyle.None : Me.Width = 700 : Me.Height = 150
+        For FadeIn As Integer = 0 To 100 Step 10
+            Me.Opacity = FadeIn / 100 : Me.Refresh() : Threading.Thread.Sleep(80)
+        Next
+        pbSplash.SendToBack() : pbSplash.Visible = False : Me.FormBorderStyle = FormBorderStyle.FixedSingle : Me.Width = 700 : Me.Height = 700 : pnlTop.Visible = True
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        For FadeOut As Int16 = 100 To 0 Step -10
+            Me.Opacity = FadeOut / 100 : Me.Refresh() : Threading.Thread.Sleep(70)
+        Next
+    End Sub
+#End Region
+
+#Region "Choose Directory"
+    Private Sub txtCSVFile_Click(sender As Object, e As EventArgs) Handles txtCSVFile.Click
+        ChooseFile()
+    End Sub
+
+    Private Sub btnChooseFile_Click(sender As Object, e As EventArgs) Handles btnChooseFile.Click
+        ChooseFile()
+    End Sub
+
+    Private Sub ChooseFile()
+        Dim fd As New OpenFileDialog
+        fd.Title = "Search for CSV File"
+        fd.Filter = "Excel files|*.XLSX;*.XLS;*.CSV"
+
+        If fd.ShowDialog() = DialogResult.OK Then
+            txtCSVFile.Text = fd.FileName
+            If Not My.Computer.FileSystem.FileExists(txtCSVFile.Text) Then
+                MsgBox("Cannot find file to be imported.")
+                Return
+            End If
+            pbLoading.Visible = True : lstResults.Items.Clear() : lstResults.Columns.Clear()
+            bg1.RunWorkerAsync(txtCSVFile.Text)
+        End If
+        fd.Dispose()
+    End Sub
+#End Region
+
+#Region "Importing CSV"
+
+    Private Sub bg1_DoWork(sender As Object, e As DoWorkEventArgs) Handles bg1.DoWork
+        Dim cvs As CSVHeaders = Nothing : Dim rowCount As Integer = 0 : Dim sValidationErrors As String = "" : Dim sLoanAmount As String = "" : Dim sOrigIntRate As String = "" : Dim sLoanOrigDate As String = "" : Dim iRowErrors As Int16 = 0
+        Dim CsvReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(e.Argument.ToString) : CsvReader.TextFieldType = FileIO.FieldType.Delimited : CsvReader.SetDelimiters(",")
+
+        While Not CsvReader.EndOfData
+            rowCount += 1 : Dim rowData() As String = CsvReader.ReadFields
+
+            If rowCount = 1 Then 'Lets do the headers first
+                cvs = New CSVHeaders(rowData) : bg1.ReportProgress(rowCount, rowData) : Continue While
+            End If
+
+            If rowData(0).ToString = "" Then Exit Sub 'Potentially the end, bail
+
+            cvs.LoadRow(rowData) : sValidationErrors = "" 'Load the row for processing, clear the errors
+
+            '--Validation--
+            If cvs.GetVal("servicer_name") = "" Then sValidationErrors += "Blank Servicer Name, "
+            If cvs.GetVal("servicer_loan_number") = "" Then sValidationErrors += "Blank Loan Number, "
+
+            'Loan Amount Validation
+            sLoanAmount = cvs.GetVal("loan_amount")
+            If sLoanAmount = "" Then
+                sValidationErrors += "Blank Loan Amount, "
+            Else
+                If IsNumeric(sLoanAmount) Then cvs.SetVal("loan_amount", CDec(sLoanAmount).ToString("C")) Else sValidationErrors += "Invalid Loan Amount of " + sLoanAmount + ", "
+            End If
+
+            'Interest Rate Validation
+            sOrigIntRate = cvs.GetVal("original_interest_rate")
+            If sOrigIntRate = "" Then
+                sValidationErrors += "Blank Interest Rate, "
+            Else
+                If IsNumeric(sOrigIntRate) Then
+                    If CDec(sOrigIntRate) > 0.2 Then
+                        sValidationErrors += "Invalid Interest Rate of " + sOrigIntRate + ", "
+                    End If
+                Else
+                    sValidationErrors += "Invalid Interest Rate of " + sOrigIntRate + ", "
+                End If
+            End If
+
+            'Origination date validation
+            sLoanOrigDate = cvs.GetVal("loan_origination_date")
+            If sLoanOrigDate = "" Then
+                sValidationErrors += "Blank Loan Date, "
+            Else
+                Dim dtLoanOrigDate As DateTime = Nothing
+                If Not DateTime.TryParseExact(sLoanOrigDate, "yyyy-MM-dd", Nothing, Globalization.DateTimeStyles.None, dtLoanOrigDate) Then
+                    sValidationErrors += "Invalid Origination Date of " + sLoanOrigDate + ", "
+                End If
+            End If
+            '--Validation Complete--
+
+
+            Dim ls As New ListViewItem((rowCount - 1).ToString) 'Item # as first column
+            For Each rw As String In rowData
+                ls.SubItems.Add(rw) 'Add to a listitem
+            Next
+
+            'Add the validation errors on the last column
+            If sValidationErrors <> "" Then sValidationErrors = Mid(sValidationErrors, 1, Len(sValidationErrors) - 2) : iRowErrors += 1
+            If ls.SubItems.Count < 7 Then ls.SubItems.Add(sValidationErrors) Else ls.SubItems(6).Text = sValidationErrors
+
+            'Hand listview item to backgroundworker progress to add
+            bg1.ReportProgress(rowCount, ls)
+        End While
+
+        e.Result = iRowErrors
+    End Sub
+
+    Private Sub bg1_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles bg1.ProgressChanged
+        If e.ProgressPercentage = 1 Then
+            lstResults.Columns.Add("Item") 'Add a column at the end for results
+            For Each rw As String In e.UserState
+                lstResults.Columns.Add(rw.ToString) 'Add all excel columns to columns in our listview
+            Next
+            lstResults.Columns.Add("Results") 'Add a column at the end for results
+        Else
+            lblStatus.Text = "Processing row# " + e.ProgressPercentage.ToString
+            lstResults.Items.Add(e.UserState)
+        End If
+    End Sub
+
+    Private Sub bg1_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles bg1.RunWorkerCompleted
+        pbLoading.Visible = False : Dim iTotal As Decimal = 0 : Dim iCount As Int16 = 0 : Dim ratevalues As New Dictionary(Of Decimal, CSVTotals)
+        lblStatus.Text = "Done Processing.  Found " + lstResults.Items.Count.ToString + " rows of data.  " + (lstResults.Items.Count - CInt(e.Result)).ToString + " valid rows.  " + e.Result.ToString + " rows in error."
+
+        For Each rw As ListViewItem In lstResults.Items
+            If rw.SubItems(6).Text = "" Then 'No errors on that line, count it in the stats
+                iTotal += rw.SubItems(3).Text : iCount += 1
+                If ratevalues.ContainsKey(rw.SubItems(4).Text) Then
+                    Dim tot As CSVTotals = ratevalues(rw.SubItems(4).Text)
+                    tot.LoanCount += 1 : tot.LoanAmount += rw.SubItems(3).Text
+                    ratevalues(rw.SubItems(4).Text) = tot
+                Else
+                    ratevalues.Add(rw.SubItems(4).Text, New CSVTotals With {.LoanCount = 1, .LoanAmount = rw.SubItems(3).Text})
+                End If
+            End If
+        Next
+
+        Dim ls As New ListViewItem With {.Text = ""} : lstResults.Items.Add(ls)
+        Dim lsTotals As New ListViewItem With {.Text = ""}
+        lsTotals.SubItems.Add("Totals") : lsTotals.SubItems.Add("Interest Rate") : lsTotals.SubItems.Add("Total Loans") : lsTotals.SubItems.Add("Total Loan Values")
+        lstResults.Items.Add(lsTotals)
+
+        For Each keyValuePair As KeyValuePair(Of Decimal, CSVTotals) In ratevalues
+            Dim lsTotalLine As New ListViewItem("") : Dim tot As CSVTotals = keyValuePair.Value
+            lsTotalLine.SubItems.Add("") : lsTotalLine.SubItems.Add(keyValuePair.Key) : lsTotalLine.SubItems.Add(tot.LoanCount) : lsTotalLine.SubItems.Add(CDec(tot.LoanAmount).ToString("C"))
+            lstResults.Items.Add(lsTotalLine)
+        Next
+
+        Dim lsFinal As New ListViewItem With {.Text = ""}
+        lsFinal.SubItems.Add("") : lsFinal.SubItems.Add("") : lsFinal.SubItems.Add(iCount.ToString) : lsFinal.SubItems.Add(CDec(iTotal).ToString("C"))
+        lstResults.Items.Add(lsFinal)
+
+        lstResults.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize)
+    End Sub
+
+    Private Sub lstResults_DrawItem(sender As Object, e As DrawListViewItemEventArgs) Handles lstResults.DrawItem
+        If e.ItemIndex Mod 2 = 0 Then  ' Even rows
+            e.DrawDefault = True  ' Draw default item content
+            e.Item.BackColor = Color.FromArgb(230, 230, 255)  ' Light blue for even rows
+        Else  ' Odd rows
+            e.DrawDefault = True
+            e.Item.BackColor = Color.FromArgb(219, 237, 252)  ' Light gray for odd rows
+        End If
+    End Sub
+
+#End Region
+
+End Class
+
+#Region "CSVTotals"
+Public Class CSVTotals
+    Public Property LoanCount As Int16
+    Public Property LoanAmount As Decimal
+End Class
+#End Region
+
+#Region "CSVHeaders"
+Public Class CSVHeaders
+    Private _ar As New ArrayList
+    Private _s() As String
+
+    Public Sub New(ByVal s() As String)
+        For q As Int16 = 0 To s.Length - 1
+            _ar.Add(s(q))
+        Next
+    End Sub
+    Public Sub LoadRow(ByVal s() As String)
+        _s = s
+    End Sub
+
+    Public Function GetVal(ByVal sColumnName As String) As String
+        For i As Int16 = 0 To _ar.Count - 1
+            If _ar(i).ToString.ToUpper = sColumnName.ToUpper Then
+                Return Trim(_s(i))
+            End If
+        Next
+
+        Return ""
+    End Function
+
+    Public Sub SetVal(ByVal sColumnName As String, ByVal sValue As String)
+        For i As Int16 = 0 To _ar.Count - 1
+            If _ar(i).ToString.ToUpper = sColumnName.ToUpper Then
+                _s(i) = sValue : Exit Sub
+            End If
+        Next
+    End Sub
+End Class
+#End Region
